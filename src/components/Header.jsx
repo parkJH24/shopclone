2367b887fb18b1a0248ecc95596343a44e81ef8e
parkJH24 <jh.parkssam@gmail.com>
2024-05-06 22:11:04 +0900

    cs수정

---
+++ src/components/Header.jsx
@@ -0,0 +1,14 @@
+/*
+nextjs에서 styled component 사용
+yarn add styled-components
+yarn add babel-plugin-styled-components
+
+*/
+
+export default function Header(){
+    return(
+        <header className="Header">
+
+        </header>
+    )
+}
