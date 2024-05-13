02ed94f65540fcfac9d168a5fc9b907773a2b6f0
parkJH24 <jh.parkssam@gmail.com>
2024-05-13 22:44:21 +0900

    재수정

--- next.config.mjs
+++ next.config.mjs
@@ -8,8 +8,6 @@ const nextConfig = {
             {
                 protocol : 'https',
                 hostname : 'firebasestorage.googleapis.com',
-                port : '',
-                pathname : '/v0/b/test-42761.appspot.com/o/**',
             }
         ]
     },
